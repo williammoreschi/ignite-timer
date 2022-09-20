58137933cdc6df8c3f028357f13f17898448b599
williammoreschi <williammoreschi@gmail.com>
2022-09-19 21:05:14 -0300

    Deixando os exemplos de lado

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-import { ButtonStyleComponent } from "./components/exemplos/csscomponents";
-import { ButtonCssModule } from "./components/exemplos/cssmodule";
-
 export function App() {
   return (
-    <>
-      <ButtonCssModule text="Primary" color="primary" />
-      <ButtonCssModule text="Secondary" color="secondary" />
-      <ButtonCssModule text="Danger" color="danger" />
-      <ButtonCssModule text="Success" color="success" />
-      <ButtonCssModule text="Default" />
-      <br />
-      <br />
-      <ButtonStyleComponent text="Primary" color="primary" />
-      <ButtonStyleComponent text="Secondary" color="secondary" />
-      <ButtonStyleComponent text="Danger" color="danger" />
-      <ButtonStyleComponent text="Success" color="success" />
-      <ButtonStyleComponent text="Default" />
-    </>
+    <><h1>Configurar tema</h1></>
   )
 }
