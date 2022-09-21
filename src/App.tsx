67c64468a732781c79944cb3f586efed38538bd2
williammoreschi <williammoreschi@gmail.com>
2022-09-20 23:18:51 -0300

    Configurando as rotas iniciais da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      <h1>Configurar tema</h1>
     </ThemeProvider>
   )
 }
